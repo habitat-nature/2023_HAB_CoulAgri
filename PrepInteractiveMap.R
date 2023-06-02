77e810a777c1286740b19eada3b522230f540359
NL-Habitat <noemie.lacroix@eco2urb.com>
2023-06-02 10:05:44 -0400

    Update PrepInteractiveMap.R

--- PrepInteractiveMap.R
+++ PrepInteractiveMap.R
@@ -2,7 +2,7 @@
 ##
 ## Prep data for interactive CartoDB map
 ################################################################################
-#date: 2023-06-02
+#date: 20230310
 #author: Kyle T. Martins & Noemie Lacroix-D. 
 
 #Objective: compile data for data package for the ECCC project
@@ -19,32 +19,51 @@ pathDataECCC <- paste0("G:/Shared drives/Projets/Actif/2022_ECCC_CoulAgri_003/",
                      "3-Analyses/1-Data/")
 pathDataJDLT <- paste0("G:/Shared drives/Projets/Actif/2022_JDT_DemForet/",
                      "3-Analyses/1-Data/")
-pathOutput <- paste0("G:/My Drive/Github/2020_ECCC_CoulAgri_002/")
-
-pathDataECCC_ph2 <- paste0("G:/Shared drives/Archive/ECCC/2020_ECCC_CoulAgri_002/",
-                         "3-Analysis/1-Data/1-GeospatialData/")
+pathOutput <- paste0("G:/My Drive/Github/2023_HAB_CagPrio/")
 
 
 ####LOAD THE DATA####
-#CAG for the BTSL East
-btsl_east <- st_read(paste0(pathDataJDLT, "CouleesAgricoles/ALL_btsl/cagFinal_inBTSL.shp"),
+# Coulees agricoles delimitations
+cag_east <- st_read(paste0(pathDataJDLT, "CouleesAgricoles/BTSL_East/cagFinal_inBTSL.shp"),
+                     quiet = T)
+cag_west <- st_read(paste0(pathDataECCC, "CouleesAgricoles/cagFinal.shp"),
                      quiet = T)
 
-#CAG for the BTSL West
-btsl_west <- st_read(paste0(pathDataECCC, "CouleesAgricoles/coulees_agricoles.shp"),
-                     quiet = T)
+#Soil data 
+soil_west <- read.csv(paste0(pathDataECCC, "Soils/cag_ProprietesSol.csv"))
+soil_east <- read.csv()
 
-# Prioritization results
-prio_west <- st_read(paste0(pathDataECCC, "Priorities/prio_bois.shp"))
+# Prioritization results 
+prio_west <- st_read(paste0(pathDataECCC, "Priorities/prio_output.shp"))
+feat_west <- st_read(pathDataECCC, "Priorities/cag_featuresRaw.shp")
+prio_east <- st_read(pathDataECCC_ph2, "Priorities/sol_prob.shp")
+
+#Administrative regions for the BTSL West and East
+admin_east <- st_read(paste0(pathDataECCC_ph2, "AdminRegion/admin_reg.shp"),
+                      quiet = T)
+admin_west <- st_read(paste0(pathDataECCC, "AdminRegion/admin_reg.shp"),
+                      quiet = T)
+
+#MRC
+mrc_east <- st_read(paste0(pathDataECCC_ph2, "AdminRegion/admin_mrc.shp"),
+                      quiet = T)
+mrc_west <- st_read(paste0(pathDataECCC, "AdminRegion/admin_mrc.shp"),
+                      quiet = T)
 
 
 ####DATA PROCESSING####
+## 1. 
 ## 1. Check which column names are not in the other
 names(btsl_west)[!names(btsl_west) %in% names(btsl_east)]
 names(btsl_east)[!names(btsl_east) %in% names(btsl_west)]
 
 
-## 2. Merge together
+## 2. Add soil data to west coulees
+summary(soil$ID %in% btsl_west$ID) # should all be true
+btsl_west <- btsl_west %>%
+  left_join(soil %>% data.frame %>% select(-geometry)) 
+
+## 3. Merge together
 # Coulees agricoles
 btsl_combined <- rbind.fill(btsl_east, btsl_west)
 names(btsl_combined)
@@ -52,29 +71,29 @@ names(btsl_combined)
 # admin region
 admin_combine <- rbind(admin_east, admin_west)
 
-# mrc
+# mrc 
 mrc_combine <- rbind(mrc_east, mrc_west)
 
 
-## 3. Reformat
-btsl_combined <- btsl_combined %>%
-  select(ID, pasl_id, super_m2, super_ha, lwVg_m2 = veg_bas_m2, lwVg_ha = veg_bas_ha, lwVg_prop = veg_bas_p,
-         selct_prio, priorite, pnte_mn_p, pnte_mn_d, argile_myn = argile_mn, limon_myn = limon_mn,
-         sable_myn = sable_mn, ph_myn = ph_mn, cec_myn = cec_mn, geometry)
-
-prio_west <- prio_west %>%
-  mutate(prio_p = priorite * 100) %>%
-  select(ID, prio_p, dist_eau_m)
+## 4. Reformat a little
+# Chande ID because some ID are present in east btsl cag and west btsl cag
+btsl_combined$ID <- 1:nrow(btsl_combined)
 
 
 ####EXPORT DATA####
-st_write(btsl_combined,
-         paste0(pathOutput, "coulees_agricoles_2023.shp"),
+st_write(btsl_combined, 
+         paste0(pathOutput, "coulees_agricoles_2023.shp"), 
          quiet = TRUE,
          delete_layer = TRUE)
 
-st_write(prio_west,
-         paste0(pathOutput, "prio_west_2023.shp"),
+st_write(admin_combine,
+         paste0(pathOutput, "admin_region_btsl.shp"), 
+         quiet = TRUE,
+         delete_layer = TRUE)
+
+st_write(mrc_combine,
+         paste0(pathOutput, "mrc_btsl.shp"), 
+         quiet = TRUE,
          delete_layer = TRUE)
 
 #End of script#
